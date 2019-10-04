b45c4afc986a4ffe8850452867b22344e90b43af
jacoloves <5511068t@gmail.com>
2019-10-04 11:01:42 +0900

    JavaPractice

---
+++ test/src/info/hello/Kata3.java
@@ -0,0 +1,19 @@
+package info.hello;
+
+public class Kata3 {
+	public static void main(String[] args) {
+		int x;
+		int x1;
+		int x2;
+
+		x1 = 10;
+		x2 = 5;
+
+		x = x1 + x2;
+		System.out.println(x);
+
+		x = x1 / x2;
+		System.out.println(x);
+	}
+
+}
---
+++ test/src/info/hello/Kata4.java
@@ -0,0 +1,15 @@
+package info.hello;
+
+public class Kata4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String x;
+		String x1 = "おはよう";
+		String x2 = "ございます";
+
+		x = x1 + x2;
+		System.out.println(x);
+	}
+
+}
